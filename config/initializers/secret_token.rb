791c466d7a3c9a1b32e3e11e650b0d60ed16a9c9
June Jung <apprentice@devbootcamp.com>
2012-08-01 10:28:21 -0700

    Initial commit | App setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Basecramp::Application.config.secret_token = '4e7bfee75b9bb0fdc2d67165f0f64fa17d056eb3d44624e8cfeef1633268c3f22323e4aa555a2afd3cf488c005fa7f07694f3fc365d41b25128addd365e42fe1'
